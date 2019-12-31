55c9a1409313f1594272c41d8aee394d6b5bb079
HeyoRockbreaker <jake.daddario@gmail.com>
2019-12-31 01:50:42 -0600

    Added baseline validations

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -2,4 +2,7 @@ class PortfolioItem < ApplicationRecord
   belongs_to :user
 
   default_scope { order(created_at: :desc) }
+
+  validates :title, presence: true, length: { maximum: 120 }
+  validates :description, presence: true, length: { maximum: 450 }
 end
