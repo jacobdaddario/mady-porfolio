db695f3de00666a3b6784d37e6e1da429f0cb003
HeyoRockbreaker <jake.daddario@gmail.com>
2020-03-25 22:29:24 -0500

    Completed styling for the blog new page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def inline_error(field, record)
+		errors = record.errors
+
+		if errors.include?(field)
+			errors.full_messages_for(field).each do |error|
+				concat content_tag(:p, error, class: 'pt-2 text-red-600 text-sm')
+			end
+		end
+
+		return nil
+	end
 end
